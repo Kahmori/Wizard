1506608dc25cef45119d92dde3542b7e180f0563
Karine Amorim <karine.amorim@squadpremium.com.br>
2024-05-11 15:30:41 -0300

    Middle size devices, responsive design

---
+++ js/caroussel.js
@@ -0,0 +1,36 @@
+const adventures = [
+  {image: './images/royal-caribbean-logo.svg', title: 'Royal Caribbean'},
+  {image: './images/costa-logo.svg', title: 'Costa'},
+  {image: './images/disney-cruise-line-logo.svg', title: 'Disney Cruise Line'},
+  {image: './images/msc-logo.svg', title: 'MSC'},
+  {image: './images/norwegian-logo.svg', title: 'Norwegian'},
+  {image: './images/oceania-logo.svg', title: 'Oceania'},
+]
+
+function renderCards(){
+
+  const cardContainer = document.getElementById('caroussel');
+
+  cardContainer.innerHTML = '';
+
+  adventures.forEach(adventure => {
+    const card = document.createElement('div');
+    
+    card.classList.add('cards');
+    card.innerHTML =
+    `<img src=${adventure.image} alt=""/>
+    <p>${adventure.title}</p>
+    `;
+
+    cardContainer.appendChild(card);
+  });
+
+  const arrow = document.createElement('div');
+  arrow.innerHTML = `<img src="./images/Arrow.svg" alt="" />`
+
+  cardContainer.appendChild(arrow);
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  renderCards();
+})
